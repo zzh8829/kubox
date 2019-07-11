95f8a074d4eeea8a59212cd4f1d6bb21d61d3398
Zihao Zhang <zzh8829@gmail.com>
2019-07-11 12:47:49 -0400

    Changes

--- kubox-init.sh
+++ kubox-init.sh
@@ -4,8 +4,10 @@ set -ex
 
 mkdir -p ~/Repos
 cd ~/Repos
-git clone https://github.com/zzh8829/dotfiles.git
-cd dotfiles
 
+rm -rf dotfiles
+git clone https://github.com/zzh8829/dotfiles.git
+
+cd dotfiles
 ./linux.sh
 ./dotfiles.sh
